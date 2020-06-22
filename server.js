06e50332a679f5929816dfa25aa302b0bbd0b9b7
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-22 15:23:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const forceSSL = function(){
         next();
     }
 }
-app.use(express.static(__dirname + './dist/voting-system-ui'));
+app.use(express.static(__dirname + '/dist/voting-system-ui'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + './dist/voting-system-ui/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/voting-system-ui/index.html'));
 });
 app.listen(process.env.PORT || 8080);
