4ef623372db2e9efaf6a2a20efb55d61af24be85
mohanasundaram <mohansundaram.info@gmail.com>
2021-07-25 00:54:33 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-import cors from "cors"
 const forceSSL = function(){
     return function(req, res, next){
         if(req.headers['x-forwarded-proto'] !== 'https'){
@@ -13,14 +12,13 @@ const forceSSL = function(){
     }
 }
 app.use(express.static(__dirname + '/dist/voting-system-UI'));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", 'https://weather-app-pwc.herokuapp.com');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
-app.use(cors({ origin: "https://weather-app-pwc.herokuapp.com/", credentials: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://weather-app-pwc.herokuapp.com');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/voting-system-UI/index.html'));
 });
