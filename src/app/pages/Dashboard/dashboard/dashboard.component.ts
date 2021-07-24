7477d45a7ba39ea2f48410f31fe89124a29379dc
mohanasundaram <mohansundaram.info@gmail.com>
2021-07-24 22:54:09 +0530

    Dashboard and Login created

--- src/app/pages/Dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/Dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataServiceService } from 'src/app/shared/services/data-service.service';
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +8,38 @@ import { DataServiceService } from 'src/app/shared/services/data-service.service
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private dataService: DataServiceService) {
+  constructor(private dataService: DataServiceService, private router: Router) {
     this.dataService.getUserData();
    }
 
   ngOnInit(): void {
   }
+  LoginFunc = (e) => {
+    this.router.navigate(['login']);
+    // // const url = "https://sleepy-brook-95490.herokuapp.com/login"
+    // const data = this.loginDetail[0];
+    // this.dataService.loginUser(url, data).subscribe(res => {
+    //   console.log('Response from server: ' + res);
+    //   console.log('Response from server: ' + res);
+    //   if (res) {
+    //     this.dataService.setUserData(res);
+    //     this.router.navigate(['login']);
+    //     // this.props.functionCallFromParent(this.state.showRegister);
+    //   }
+    // });
+}
+    //   axios.post(url, data).then(res => {
+    //   // this.props.functionCallFromParent(this.state.showRegister);
+    //   console.log('Response from server: ' + res);
+    //   console.log('Response from server: ' + res.data.user);
+    //   if (res.data.user) {
+    //     this.router.navigate(['dashboard', {data: res}]);
+    //     // this.props.functionCallFromParent(this.state.showRegister);
+    //   }
+    // }).catch(err => {
+    //   console.log ('Error of post:' + err);
+    //  // this.props.functionCallFromParent(this.state.showRegister);
+
+    // });
 
 }
--- src/app/pages/Login/login/login.component.ts
+++ src/app/pages/Login/login/login.component.ts
@@ -14,33 +14,4 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  LoginFunc = (e) => {
-    // this.props.functionCallFromParent(this.state.showRegister);
-    //const url = 'http://voting-assign.herokuapp.com/http://voting-front-end.herokuapp.com/login/';
-    //const url = 'http://voting-assign.herokuapp.com/login/';
-    const url = "https://sleepy-brook-95490.herokuapp.com/login"
-    const data = this.loginDetail[0];
-    this.dataService.loginUser(url, data).subscribe(res => {
-      console.log('Response from server: ' + res);
-      console.log('Response from server: ' + res);
-      if (res) {
-        this.dataService.setUserData(res);
-        this.router.navigate(['dashboard']);
-        // this.props.functionCallFromParent(this.state.showRegister);
-      }
-    });
-    // axios.post(url, data).then(res => {
-    //   // this.props.functionCallFromParent(this.state.showRegister);
-    //   console.log('Response from server: ' + res);
-    //   console.log('Response from server: ' + res.data.user);
-    //   if (res.data.user) {
-    //     this.router.navigate(['dashboard', {data: res}]);
-    //     // this.props.functionCallFromParent(this.state.showRegister);
-    //   }
-    // }).catch(err => {
-    //   console.log ('Error of post:' + err);
-    //  // this.props.functionCallFromParent(this.state.showRegister);
-
-    // });
-}
 }
