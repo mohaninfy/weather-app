f1bdb4d238f222e219d24e535c11729d90b4610d
mohanasundaram <mohansundaram.info@gmail.com>
2021-07-25 00:40:45 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ const forceSSL = function(){
         next();
     }
 }
+app.use(express.static(__dirname + '/dist/voting-system-UI'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
+});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/voting-system-UI/index.html'));
 });
 app.listen(process.env.PORT || 8080);
