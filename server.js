6b5f5d049ed843553c8b63b635f16d7be0b2addd
mohanasundaram <mohansundaram.info@gmail.com>
2021-07-25 07:47:56 +0530

    cors added in package json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+import cors from 'cors';
 // const forceSSL = function () {
 //     return function (req, res, next) {
 //         if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -11,21 +12,22 @@ const path = require('path');
 //         next();
 //     }
 // }
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
-    next()
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested, Content-Type, Accept Authorization"
+//     )
+//     if (req.method === "OPTIONS") {
+//         res.header(
+//             "Access-Control-Allow-Methods",
+//             "POST, PUT, PATCH, GET, DELETE"
+//         )
+//         return res.status(200).json({})
+//     }
+//     next()
+// });
 app.use(express.static(__dirname + '/dist/voting-system-UI'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/voting-system-UI/index.html'));
